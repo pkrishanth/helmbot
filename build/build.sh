58cbc2c398721a97a2d0c424a14f3f542fdc63a5
Krishnath Poologanathan <pkrishnath@gmail.com>
2017-04-15 11:17:02 +0100

    used alipine to run helmbot

--- build/build.sh
+++ build/build.sh
@@ -1,22 +1,6 @@
-#!/bin/sh
-
-# Copyright 2016 The Kubernetes Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 set -o errexit
 set -o nounset
-#set -o pipefail
+
 
 if [ -z "${PKG}" ]; then
     echo "PKG must be set"
@@ -34,8 +18,6 @@ fi
 export CGO_ENABLED=0
 export GOARCH="${ARCH}"
 
+curl https://glide.sh/get | sh
 
-go install                                                         \
-    -installsuffix "static"                                        \
-    -ldflags "-X ${PKG}/pkg/version.VERSION=${VERSION}"            \
-    ./...
+go install $(glide novendor) 
